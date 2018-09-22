5b9f557028f16e87c3df4d879aa3e2179f086907
Rohan Agarwal <rohanx96@gmail.com>
2018-09-22 17:17:22 +0530

    Add react native camera dependency Create layout for home page Hide navigation bar and status bar

--- App.js
+++ App.js
@@ -6,24 +6,129 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Picker,
+  TouchableHighlight
+} from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+type State = {};
+let defaultState = {
+  selectedPrimaryFilter: undefined,
+  selectedSecondaryFilter: undefined
+};
+
+let primaryFilter = [
+  "Select primary filter",
+  "First",
+  "Second",
+  "Third",
+  "Fourth",
+  "Fifth"
+];
+
+let secondaryFilter = ["SecFirst", "SecSecond", "SecThird"];
+export default class App extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = defaultState;
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.camera} />
+        <View style={{ flex: 0.25 }}>
+          <View style={{ flexDirection: "row", backgroundColor: "#ddd" }}>
+            <Picker
+              style={styles.picker}
+              selectedValue={this.state.selectedPrimaryFilter}
+              mode="dropdown"
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ selectedPrimaryFilter: itemValue })
+              }
+            >
+              {primaryFilter.map((data, index) => (
+                <Picker.Item label={data} value={data} />
+              ))}
+            </Picker>
+            <Picker
+              style={styles.picker}
+              mode="dropdown"
+              selectedValue={this.state.selectedSecondaryFilter}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ selectedSecondaryFilter: itemValue })
+              }
+              enabled={this.state.selectedPrimaryFilter ? true : false}
+            >
+              {secondaryFilter.map((data, index) => (
+                <Picker.Item label={data} value={data} />
+              ))}
+            </Picker>
+          </View>
+          <View
+            style={{ flex: 1, flexDirection: "row", alignItems: "stretch" }}
+          >
+            <TouchableHighlight
+              style={{
+                backgroundColor: "#018786",
+                padding: 12,
+                flex: 0.55,
+                justifyContent: "center"
+              }}
+              onPress={() => {}}
+            >
+              <Text
+                style={{ color: "#fff", fontSize: 24, textAlign: "center" }}
+              >
+                {"TAKE & \nSEND"}
+              </Text>
+            </TouchableHighlight>
+            <View style={{ flexDirection: "column", flex: 0.45 }}>
+              <TouchableHighlight
+                style={{
+                  backgroundColor: "#666",
+                  padding: 12,
+                  flex: 1,
+                  justifyContent: "center"
+                }}
+                onPress={() => {}}
+              >
+                <Text
+                  style={{ color: "#fff", fontSize: 16, textAlign: "center" }}
+                >
+                  {"CLEAR"}
+                </Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={{
+                  backgroundColor: "#f50057",
+                  padding: 12,
+                  flex: 1,
+                  justifyContent: "center"
+                }}
+                onPress={() => {}}
+              >
+                <Text
+                  style={{ color: "#fff", fontSize: 16, textAlign: "center" }}
+                >
+                  {"EXIT"}
+                </Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -32,18 +137,26 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: "#F5FCFF"
+  },
+  camera: {
+    flex: 0.75,
+    backgroundColor: "#000"
+  },
+  picker: {
+    flex: 0.5
+  },
+  pickerItem: {
+    paddingLeft: 16
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
--- android/app/src/main/java/com/filteredimageupload/MainActivity.java
+++ android/app/src/main/java/com/filteredimageupload/MainActivity.java
@@ -1,8 +1,44 @@
 package com.filteredimageupload;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideNavigationBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+
+    void hideNavigationBar(){
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
